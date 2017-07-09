ad2b778189aa85f03ffd98572eb028f07ee507e6
GuMiner <gus.gran@gmail.com>
2017-07-09 16:10:46 -0700

    Finishing all the lines of the test cube.

--- TestCube.cpp
+++ TestCube.cpp
@@ -4,13 +4,25 @@
 TestCube::TestCube() : renderer(new LineRenderer(false))
 {
     // Define a cube going from black (X-, Y-, Z-) to RGB (X+, Y+, Z+), (-1, -1, -1) to (1, 1, 1)
+
+    // 3 lines from -1, -1, -1
     renderer->AddLine(glm::vec3(-1, -1, -1), glm::vec3(0, 0, 0), glm::vec3(1, -1, -1), glm::vec3(1, 0, 0));
     renderer->AddLine(glm::vec3(-1, -1, -1), glm::vec3(0, 0, 0), glm::vec3(-1, 1, -1), glm::vec3(0, 1, 0));
     renderer->AddLine(glm::vec3(-1, -1, -1), glm::vec3(0, 0, 0), glm::vec3(-1, -1, 1), glm::vec3(0, 0, 1));
 
+    // Intermediate lines
     renderer->AddLine(glm::vec3(1, 1, -1), glm::vec3(1, 1, 0), glm::vec3(1, -1, -1), glm::vec3(1, 0, 0));
     renderer->AddLine(glm::vec3(-1, 1, 1), glm::vec3(0, 1, 1), glm::vec3(-1, 1, -1), glm::vec3(0, 1, 0));
     renderer->AddLine(glm::vec3(1, -1, 1), glm::vec3(1, 0, 1), glm::vec3(-1, -1, 1), glm::vec3(0, 0, 1));
+
+    renderer->AddLine(glm::vec3(1, -1, 1), glm::vec3(1, 0, 1), glm::vec3(1, -1, -1), glm::vec3(1, 0, 0));
+    renderer->AddLine(glm::vec3(1, 1, -1), glm::vec3(1, 1, 0), glm::vec3(-1, 1, -1), glm::vec3(0, 1, 0));
+    renderer->AddLine(glm::vec3(-1, 1, 1), glm::vec3(0, 1, 1), glm::vec3(-1, -1, 1), glm::vec3(0, 0, 1));
+
+    // 3 lines from 1, 1, 1
+    renderer->AddLine(glm::vec3(1, 1, -1), glm::vec3(1, 1, 0), glm::vec3(1, 1, 1), glm::vec3(1, 1, 1));
+    renderer->AddLine(glm::vec3(-1, 1, 1), glm::vec3(0, 1, 1), glm::vec3(1, 1, 1), glm::vec3(1, 1, 1));
+    renderer->AddLine(glm::vec3(1, -1, 1), glm::vec3(1, 0, 1), glm::vec3(1, 1, 1), glm::vec3(1, 1, 1));
 
     renderer->Update();
 }
--- version.h
+++ version.h
@@ -6,7 +6,7 @@
 namespace AutoVersion
 {
     static const long MAJOR_VERSION = 0;
-    static const long MINOR_VERSION = 66;
+    static const long MINOR_VERSION = 68;
 
     static std::string GetVersionString(std::string name)
     {
