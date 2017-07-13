4f4ac64e65014096d5c1bf8b9ce0dad1d0620b52
GuMiner <gus.gran@gmail.com>
2017-07-12 20:21:20 -0700

    Cleaning up the directional sun to be more realistic, breaking apart fog and gamma correction into fragment shader fragment files.

--- shaders/shadedTerrain.fs
+++ shaders/shadedTerrain.fs
@@ -1,5 +1,8 @@
 #version 400 core
 #include "fullScreenQuad/rayComputation.fs_frag"
+#include "fullScreenQuad/gammaCorrection.fs_frag"
+#include "fullScreenQuad/fog.fs_frag"
+#include "fullScreenQuad/directionalSun.fs_frag"
 
 uniform vec3 cameraPosition;
 
@@ -7,19 +10,12 @@ out vec4 color;
 
 const float maxDistance = 500.0f;
 const vec3 terrainColor = vec3(0.2f, 0.2f, 0.2f);
-const vec3 sunPosition = vec3(0.0f, 0.0f, 10.0f);
-const vec3 sunColor = vec3(1.0f, 0.85f, 0.80f);
-const vec3 fogColor = vec3(0.53f, 0.6f, 0.65f);
+
 
 // Render a basic ray-traced basic bumpy terrain.
 float getHeight(vec2 point)
 {
-    return sin(point.x * 3) * sin(point.y * 3) * 0.5f - 1.0f + cos(point.x * 10) * 0.25f;
-}
-
-vec4 applyGammaCorrection(vec4 color)
-{
-    return vec4(pow(color.xyz, vec3(0.4545)), 1.0);
+    return sin(point.x * 3) * sin(point.y * 3) * 0.5f - 1.0f;
 }
 
 vec3 getNormal(float height, vec2 point, float delta)
@@ -27,14 +23,6 @@ vec3 getNormal(float height, vec2 point, float delta)
     float xDelta = getHeight(vec2(point.x - delta, point.y)) - getHeight(vec2(point.x + delta, point.y));
     float zDelta = getHeight(vec2(point.x, point.y - delta)) - getHeight(vec2(point.x, point.y + delta));
     return normalize(vec3(xDelta, 2.0f * delta, zDelta));
-}
-
-vec3 mixFog(vec3 color, float distance)
-{
-    const float exponentialFactor = -0.01f;
-    float fogAmount = 1 - exp(distance * exponentialFactor);
-    
-    return mix(color, fogColor, fogAmount);
 }
 
 bool castRay(vec3 eye, vec3 dir, inout vec3 intersectionPoint)
@@ -77,9 +65,8 @@ void main(void)
     if (didHitTerrain)
     {
         float cameraDistance = distance(cameraPosition, intersectionPoint);
-        vec3 sunDirection = normalize(intersectionPoint - sunPosition);
         vec3 normal = getNormal(intersectionPoint.y, intersectionPoint.xz, 0.01f);
-        vec3 baseColor = max(0.0f, dot(sunDirection, normal)) * terrainColor;
+        vec3 baseColor = computeSolarStrength(normal, terrainColor);
         
         color = vec4(mixFog(baseColor, cameraDistance), 1.0f);
     }
