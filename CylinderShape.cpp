43d9aa19b9c13ea302f038f6511560aaf263ae80
GuMiner <gus.gran@gmail.com>
2017-09-08 21:42:06 -0700

    Adding a plane fan for the top of the cylinder. Work still needs to be done to fix the plane fan location, finish the lower part of the cylinder, etc.

--- CylinderShape.cpp
+++ CylinderShape.cpp
@@ -23,19 +23,27 @@ CoreGeometry CylinderShape::Generate()
     // Generate the sides of the cylinder
     float midRadius = (upperRadius + lowerRadius) / 2.0f;
     float sliceSize = 2.0f * (float)M_PI / angularSteps;
+
+    float cylinderTiltAngle = std::atan(std::abs(upperRadius - lowerRadius) / 2.0f);
+    // Our cylinder is really a capped polygon. With low angular resolution, we end up with other interesting shapes
+    float innerPolygonHalfAngle = ((float)(angularSteps - 2) * (float)M_PI) / (2.0f * angularSteps);
+    float planeSideDistance = midRadius / std::tan(innerPolygonHalfAngle);
+
     for (int i = 0; i < angularSteps; i++)
     {
         // The angle the plane is to be rotated to.
         float angle = sliceSize * (float)i;
 
-        // Our cylinder is really a capped polygon. With low angular resolution, we end up with other interesting shapes
-        float innerPolygonHalfAngle = ((float)(angularSteps - 2) * (float)M_PI) / (2.0f * angularSteps);
-        float planeSideDistance = midRadius / std::tan(innerPolygonHalfAngle);
-
         // Each plane needs to be angled correctly to meet both the lower and upper radius, along with proper skewing of both the top and the bottom
         PlanePrimitive::AddPlane(vertices, normals,
-            glm::rotate(angle, glm::vec3(0, 1, 0)) * glm::scale(glm::vec3(planeSideDistance, 1.0f, 1.0f)), glm::vec3(0, 0, 1.0f - midRadius)); // Rotate to the correct angle.
+            glm::rotate(cylinderTiltAngle, glm::vec3(1, 0, 0)) *  // TODO, we need to move the plane to 0,0,0 to perform this transformation ... and then back.
+            glm::rotate(angle, glm::vec3(0, 1, 0)) * 
+            glm::scale(glm::vec3(planeSideDistance, 1.0f, 1.0f)), glm::vec3(0, 0, 1.0f - midRadius)); // Rotate to the correct angle.
     }
+
+    // Generate the tops and bottoms of the cylinder.
+    glm::mat4 planeFanTranslation = glm::translate(glm::vec3(0, 1.0f, 0)) * glm::scale(glm::vec3(midRadius, 1.0f, midRadius));
+    PlanePrimitive::AddPlaneFan(vertices, normals, angularSteps, planeFanTranslation); // TODO need to transform to top and bottom.
     
     coreGeometry.SetGeometryData(vertices, normals);
     return coreGeometry;
--- PlanePrimitive.cpp
+++ PlanePrimitive.cpp
@@ -1,3 +1,6 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <glm\gtx\transform.hpp>
 #include "PlanePrimitive.h"
 
 void PlanePrimitive::AddPlane(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& normals, glm::mat3 rotateScaleTransform, glm::vec3 translation)
@@ -25,5 +28,37 @@ void PlanePrimitive::AddPlane(std::vector<glm::vec3>& vertices, std::vector<glm:
         int itemIdx = (int)vertices.size() - (i + 1);
         vertices[itemIdx] = rotateScaleTransform * (vertices[itemIdx] + translation);
         normals[itemIdx] = rotateScaleTransform * normals[itemIdx];
+    }
+}
+
+void PlanePrimitive::AddPlaneFan(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& normals, int segments, glm::mat4 transform)
+{
+    glm::vec3 normal = glm::vec3(0, 1, 0);
+
+    float angleStep = (float)(2.0f * M_PI) / (float)segments;
+    glm::vec3 currentVector = glm::vec3(0, 0, -1);
+    currentVector = glm::mat3(glm::rotate(-angleStep / 2.0f, normal)) * currentVector;
+
+    glm::mat3 angularRotationMatrix = glm::rotate(-angleStep, glm::vec3(0, 1, 0));
+    for (int i = 0; i < segments; i++)
+    {
+        vertices.push_back(glm::vec3(0, 0, 0));
+        normals.push_back(normal);
+
+        vertices.push_back(currentVector);
+        normals.push_back(normal);
+        
+        currentVector = angularRotationMatrix * currentVector;
+        vertices.push_back(currentVector);
+        normals.push_back(normal);
+    }
+
+    // Loopback six items and rotate as applicable.
+    for (int i = 0; i < segments * 3; i++)
+    {
+        int itemIdx = (int)vertices.size() - (i + 1);
+
+        vertices[itemIdx] = glm::vec3(transform * glm::vec4(vertices[itemIdx].x, vertices[itemIdx].y, vertices[itemIdx].z, 1.0f));
+        normals[itemIdx] = glm::mat3(transform) * normals[itemIdx];
     }
 }
